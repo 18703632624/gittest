5fbc2d5b3ea0e0131117cedbba818bf0fdf25bfe
18703632624 <73087670+18703632624@users.noreply.github.com>
2021-06-18 17:01:45 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello git555");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
